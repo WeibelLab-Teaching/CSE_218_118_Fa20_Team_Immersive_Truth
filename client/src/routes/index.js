3d75543a8cfc3b6a5e1cfa9c2d153230d66127bc
Yongze Yao <yongzeyao@gmail.com>
2020-12-04 00:44:23 -0800

    Implement WebRTC on front-end.

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import Home from '../views/Home.vue';
 import Join from '../views/Join.vue';
-import Room from '../views/Room.vue';
 import Rules from '../views/Rules.vue';
 import Host from '../views/Host.vue';
 import Name from '../views/Name.vue';
@@ -14,7 +13,6 @@ export default createRouter({
   routes: [
     { path: '/', component: Home },
     { path: '/join', component: Join },
-    { path: '/room/:roomId/name/:name', component: Room },
     { path: '/rules', component: Rules },
     { path: '/host', component: Host },
     { path: '/name', component: Name },
