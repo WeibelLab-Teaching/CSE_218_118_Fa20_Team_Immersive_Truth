fed7042799dbd504626799857d0397a96efd0fb1
Yongze Yao <yongzeyao@gmail.com>
2020-12-07 10:10:02 -0800

    add adapter.js

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import './index.scss';
+import adapter from 'webrtc-adapter';
+
+console.log(adapter.browserDetails.browser);
 
 import router from './routes';
 import { store, key } from './store';
