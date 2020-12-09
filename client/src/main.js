1aac0073f3fe3bb2e396e2fe0c144ace67b48ee4
Yongze Yao <yongzeyao@gmail.com>
2020-12-08 17:45:54 -0800

    remove webrtc-adapter

--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import './index.scss';
-import adapter from 'webrtc-adapter';
-
-console.log(adapter.browserDetails.browser);
 
 import router from './routes';
 import { store, key } from './store';
